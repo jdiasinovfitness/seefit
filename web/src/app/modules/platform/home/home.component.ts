a4868b21c05cdac4fc6ed74073f4bf3881d63e10
JoseDias <jose.dias@inovretail.com>
2022-09-27 15:12:54 +0100

    comments and testing pipeline

--- web/src/app/modules/platform/home/home.component.ts
+++ web/src/app/modules/platform/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {}
 
-
+  //set activate language
   public setActiveLang(lang: string) {
     this.transloco.setActiveLang(lang);
   }
