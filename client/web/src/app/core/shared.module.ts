a5f5dd1086954d632a6204b7f6777b4ed9341c3e
Axel <axelferreira@me.com>
2023-02-28 11:55:25 +0000

    feat: refactoring project

--- client/web/src/app/core/shared.module.ts
+++ client/web/src/app/core/shared.module.ts
@@ -4,20 +4,18 @@ import { AuthService } from './services/auth.service';
 import { GlobalStorage } from './storage/global.storage';
 import { ConfigService } from './services/config.service';
 import { MaterialExampleModule } from '../material.module';
-import { InputComponent } from './components/input/input.component';
-import { ButtonComponent } from './components/button/button.component';
 import { UiComponentsModule } from '@i9r/ui-components';
 import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
-	declarations: [InputComponent, ButtonComponent],
+	declarations: [],
 	imports: [
 		CommonModule,
 		MaterialExampleModule,
 		UiComponentsModule,
 		TranslocoModule,
 	],
-	exports: [InputComponent, ButtonComponent, TranslocoModule],
+	exports: [TranslocoModule],
 	providers: [AuthService, GlobalStorage, ConfigService],
 })
 export class SharedModule {}
--- client/web/src/app/modules/platform/platform.module.ts
+++ client/web/src/app/modules/platform/platform.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { PlatformRoutingModule } from './platform-routing.module';
-import { TranslocoModule, TRANSLOCO_SCOPE } from '@ngneat/transloco';
+import { TRANSLOCO_SCOPE } from '@ngneat/transloco';
 import { loader } from 'src/app/core/i18n/transloco-loader';
 import { PlatformComponent } from './platform.component';
 import { MaterialExampleModule } from 'src/app/material.module';
-import { AuthService } from 'src/app/core/services/auth.service';
 import { SharedModule } from 'src/app/core/shared.module';
 
 @NgModule({
