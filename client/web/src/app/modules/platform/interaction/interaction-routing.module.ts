925161b2bd4ccedf3e5a63a8fc81bca97f6f7743
Axel <axelferreira@me.com>
2023-02-28 09:36:55 +0000

    feat: adding interaction component

---
+++ client/web/src/app/modules/platform/interaction/interaction-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InteractionComponent } from './interaction.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: InteractionComponent,
+	},
+];
+
+@NgModule({
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
+})
+export class InteractionRoutingModule {}
