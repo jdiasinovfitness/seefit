01f2a86e73937a24a5742dead63e28e84060c287
Axel <axelferreira@me.com>
2023-03-02 11:27:56 +0000

    feat: adding model

--- client/web/src/app/core/interfaces/icidata.ts
+++ client/web/src/app/core/interfaces/icidata.ts
@@ -6,10 +6,32 @@ export interface ICIData {
 	primary: ICILabel;
 	secondary: ICILabel;
 	imageUrl: string;
+	consumerInfo: ICIConsumerInfo;
 }
 
 export interface ICILabel {
 	label: string;
 	value: string;
 	color: string;
+}
+
+export interface ICIConsumerInfo {
+	customerRecord: ICICustomerRecord;
+	contractType: ICICustomerRecord;
+	additionalInfo: ICIAdditionalInfo;
+}
+export interface ICICustomerRecord {
+	title: string;
+	content: Array<ICIKeyVal>;
+}
+
+export interface ICIKeyVal {
+	key: string;
+	value: string;
+}
+
+export interface ICIAdditionalInfo {
+	title: string;
+	lifeCycle: ICIKeyVal;
+	icons: Array<string>;
 }
