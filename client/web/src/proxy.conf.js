b8517b44a079ea09fc94751b3eae7261291499a9
Axel <axelferreira@me.com>
2023-03-09 14:44:26 +0000

    fix: rebuilding package lock

--- client/web/src/proxy.conf.js
+++ client/web/src/proxy.conf.js
@@ -1,13 +1,13 @@
 const TRG =
-	process.env.npm_lifecycle_event === 'start:dev' ? process.env.DEV_USER : 'tst';
+  process.env.npm_lifecycle_event === 'start:dev' ? process.env.DEV_USER : 'tst';
 
 const PROXY_CONFIG = {
-	'/middleware/*': {
-		target: `https://${TRG}-seefit.inovfitness.com`,
-		secure: true,
-		logLevel: 'debug',
-		changeOrigin: true,
-	},
+  '/middleware/*': {
+    target: `https://${TRG}-seefit.inovfitness.com`,
+    secure: true,
+    logLevel: 'debug',
+    changeOrigin: true,
+  },
 };
 
 module.exports = PROXY_CONFIG;
