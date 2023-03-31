7590b846dbc1a8f78d98a33a698ef7649dc4d600
Axel <axelferreira@me.com>
2023-03-31 11:04:46 +0100

    fix: configurations

--- client/web/capacitor.config.ts
+++ client/web/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter', // TODO: check this
-  appName: 'seeFit',
+  appName: 'seefit',
   webDir: 'www',
   bundledWebRuntime: false,
 };
