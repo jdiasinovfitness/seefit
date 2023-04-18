5a91dac61d37ae6e6a881e53f6385d4b941bb005
JoseDias <jose.dias@inovretail.com>
2023-04-18 18:47:22 +0100

    feat: update translation assets folder path

--- client/web/src/environments/environment.prod.ts
+++ client/web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: '/middleware',
-  i18nURL: `http://127.0.0.1:8080`, // FIXME: replace with service url
+  i18nURL: `../assets/i18n`, // FIXME: replace with service url
   production: true,
 };
--- client/web/src/environments/environment.ts
+++ client/web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: '/middleware',
-  i18nURL: `http://127.0.0.1:8080`, // `../assets/i18n`
+  i18nURL: `../assets/i18n`, // `../assets/i18n`
   production: false,
 };
