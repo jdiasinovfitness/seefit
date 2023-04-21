cdf77546ee50c7fad16416a62e25b279c60ef95d
Axel <axelferreira@me.com>
2023-04-21 09:38:44 +0100

    feat: impl. origin selection

--- client/seeFit/src/app/modules/platform/platform.component.ts
+++ client/seeFit/src/app/modules/platform/platform.component.ts
@@ -3,6 +3,7 @@ import { MenuController } from '@ionic/angular';
 import { AuthService } from '../../core/services/auth.service';
 import { DataService } from '../../core/services/data.service';
 import { MenuData } from '../../core/interfaces/menu.model';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-platform',
@@ -13,15 +14,39 @@ export class PlatformComponent implements OnInit {
   profilePhoto = 'https://ionicframework.com/docs/img/demos/avatar.svg';
   userName = 'John Doe';
   public menuItems!: Array<MenuData>;
+  public originList!: Array<any>;
+  selectedOrigin!: number;
 
   constructor(
     private menu: MenuController,
     private authService: AuthService,
-    private dataService: DataService
+    public dataService: DataService,
+    private modalCtrl: ModalController
   ) {}
 
   ngOnInit(): void {
     this.menuItems = this.dataService.menuItems;
+    this.originList = this.dataService.originList;
+    this.selectedOrigin = this.dataService.selectedOrigin;
+  }
+
+  onOriginSelect(event: any) {
+    const val = event.target.value;
+    this.selectedOrigin = val ? val : this.selectedOrigin;
+  }
+
+  cancel() {
+    this.selectedOrigin = this.dataService.selectedOrigin;
+    this.modalCtrl.dismiss();
+  }
+
+  confirm() {
+    this.modalCtrl.dismiss();
+    this.dataService.selectedOrigin = this.selectedOrigin;
+  }
+
+  onWillDismiss(event: Event) {
+    this.menu.close();
   }
 
   toggleMenu() {
