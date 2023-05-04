c9360eadfbb20bca45efb33b83b5ec054119a0b4
Axel <axelferreira@me.com>
2023-05-04 16:38:42 +0100

    feat: adding user sub routes components

---
+++ client/seeFit/src/app/modules/platform/user/activity/activity.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivityRoutingModule } from './activity-routing.module';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/core/shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ActivityRoutingModule, SharedModule, FormsModule],
+})
+export class ActivityModule {}
--- client/seeFit/src/app/modules/platform/user/profile/profile.module.ts
+++ client/seeFit/src/app/modules/platform/user/profile/profile.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { SharedModule } from 'src/app/core/shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, SharedModule, FormsModule],
+  imports: [CommonModule, ProfileRoutingModule, SharedModule, FormsModule],
 })
 export class ProfileModule {}
