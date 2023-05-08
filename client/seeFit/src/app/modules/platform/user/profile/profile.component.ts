923284a1005e28eddc84b1305a16274050be3b6f
Axel <axelferreira@me.com>
2023-05-08 16:39:55 +0100

    feat: impl profile form

--- client/seeFit/src/app/modules/platform/user/profile/profile.component.ts
+++ client/seeFit/src/app/modules/platform/user/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AuthInfo,
+  LangInfo,
+} from '../../../../core/interfaces/auth-info.model';
+import { UserService } from '../../../../core/services/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,11 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+  userData: AuthInfo;
+  newName!: string;
+  langList!: Array<LangInfo>;
+  userForm: FormGroup;
+
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) {
+    this.userData = userService.user;
+    this.langList = [
+      { id: 'pt-PT', name: 'Portugues' },
+      { id: 'en-EN', name: 'English' },
+    ];
+    this.userForm = this.formBuilder.group({
+      name: [userService.user.name, [Validators.required]],
+      lang: [userService.getCurrentLang(), [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+    setTimeout(() => {
+      console.log(
+        'userService.getCurrentLang()',
+        this.userService.getCurrentLang()
+      ); // TODO: Remove on PR!
+    }, 2000);
+  }
 
   ngOnInit() {}
 
   onNameKeyUp(event: any) {
     console.log('NNNN', event); // TODO: Remove on PR!
+  }
+
+  onPwdKeyUp(event: any) {
+    const a = event.target.value;
+    console.log('NNNN', event, a, a.trim()); // TODO: Remove on PR!
   }
 }
--- client/seeFit/src/app/modules/platform/user/profile/profile.module.ts
+++ client/seeFit/src/app/modules/platform/user/profile/profile.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { SharedModule } from 'src/app/core/shared/shared.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule, SharedModule, FormsModule],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ProfileModule {}
