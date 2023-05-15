d8090847bfe6ffcec8d258fdd936ba7547178f0b
Axel <axelferreira@me.com>
2023-05-15 15:57:14 +0100

    feat: removing unused lib

--- client/seeFit/src/app/core/shared/shared.module.ts
+++ client/seeFit/src/app/core/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { AuthService } from '../services/auth.service';
 import { GlobalStorage } from '../storage/global.storage';
 import { ConfigService } from '../services/config.service';
 import { SafeHtmlPipe } from '../pipes/safe-html.pipe';
-import { UiComponentsModule } from '@i9r/ui-components';
 import { IonicModule } from '@ionic/angular';
 
 @NgModule({
@@ -13,10 +12,9 @@ import { IonicModule } from '@ionic/angular';
   imports: [
     CommonModule,
     TranslateModule.forChild({ extend: true }),
-    UiComponentsModule,
     IonicModule,
   ],
-  exports: [TranslateModule, SafeHtmlPipe, UiComponentsModule, IonicModule],
+  exports: [TranslateModule, SafeHtmlPipe, IonicModule],
   providers: [AuthService, GlobalStorage, ConfigService],
 })
 export class SharedModule {}
