c5ea3b51df3e08203801c8759f4ceb49a54e5342
Axel <axelferreira@me.com>
2023-05-22 15:57:18 +0100

    feat: impl notification screen

--- client/seeFit/src/app/modules/platform/user/notifications/notifications.component.ts
+++ client/seeFit/src/app/modules/platform/user/notifications/notifications.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { NotificationData } from '../../../../core/interfaces/notification.model';
 import { UserService } from 'src/app/core/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-notifications',
@@ -8,10 +9,26 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./notifications.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class NotificationsComponent implements OnInit {
-  // notificationList: Array<NotificationData>;
+export class NotificationsComponent implements OnDestroy {
+  notificationList!: Array<NotificationData> | undefined;
+  notificationSub!: Subscription;
 
-  constructor(public userService: UserService) {}
+  constructor(public userService: UserService) {
+    this.notificationSub = this.userService.notification$.subscribe((res) => {
+      res?.forEach((el) => (el.isOpen = false));
+      this.notificationList = res;
+    });
+  }
 
-  ngOnInit() {}
+  ngOnDestroy() {
+    this.notificationSub.unsubscribe();
+  }
+
+  expand(index: number) {
+    if (!this.notificationList) {
+      return;
+    }
+    this.notificationList[index].isOpen =
+      !this.notificationList[index].isOpen || false;
+  }
 }
