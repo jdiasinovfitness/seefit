efcf096805a2a046b531608fcccb2d0a41fcfe28
Axel <axelferreira@me.com>
2023-07-11 16:56:04 +0100

    feat: customer view

--- client/web/src/app/modules/platform/customer/customer.component.ts
+++ client/web/src/app/modules/platform/customer/customer.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { firstValueFrom } from 'rxjs';
+import { THRESHOLDS } from 'src/app/core/constants/config.constants';
 import { ICIData } from 'src/app/core/interfaces/icidata.model';
 import { DataService } from 'src/app/core/services/data.service';
 
@@ -16,22 +19,34 @@ export enum Phases {
   styleUrls: ['./customer.component.scss'],
 })
 export class CustomerComponent implements OnInit {
+  @HostListener('window:resize', ['$event']) onResize() {
+    this.isMobile = window.innerWidth >= THRESHOLDS.MOBILE_THRESHOLD;
+  }
   phaseEnum = Phases;
   currentPhase = Phases.loading;
 
   customer!: ICIData;
   customerId!: string;
+  tabList!: Array<any>;
+
+  innerWidth!: number;
+
+  currentTab = '0';
+  isMobile!: boolean; // FIXME: set according to screen size
 
   constructor(
     private dataService: DataService,
     private activeRoute: ActivatedRoute,
+    private translateService: TranslateService,
     private router: Router,
   ) {
     this.customerId = this.activeRoute.snapshot.params['id'];
   }
 
   ngOnInit() {
+    this.isMobile = window.innerWidth >= THRESHOLDS.MOBILE_THRESHOLD;
     this.loadData();
+    this.getTabs();
   }
 
   async loadData() {
@@ -52,4 +67,54 @@ export class CustomerComponent implements OnInit {
         });
     });
   }
+
+  onTabChange(event: any, index: number) {
+    console.log('AAA', event, index); // TODO: Remove on PR!
+  }
+
+  prevTab(event: any) {
+    const newTab = Number(this.currentTab) > 0 ? (Number(this.currentTab) - 1).toString() : this.currentTab;
+    this.currentTab = newTab;
+  }
+
+  nextTab(event: any) {
+    const newTab = Number(this.currentTab) < this.tabList.length - 1 ? (Number(this.currentTab) + 1).toString() : this.currentTab;
+    this.currentTab = newTab;
+
+  }
+
+
+  async getTabs() {
+    this.tabList = [
+      {
+        id: '0',
+        disabled: false,
+        title: await firstValueFrom(
+          this.translateService.get('customer.tabs.information.title')
+        ),
+      },
+      {
+        id: '1',
+        disabled: false,
+        title: await firstValueFrom(
+          this.translateService.get('customer.tabs.lifecycle.title')
+        ),
+      },
+      {
+        id: '2',
+        disabled: false,
+        title: await firstValueFrom(
+          this.translateService.get('customer.tabs.history.title')
+        ),
+      },
+      {
+        id: '3',
+        disabled: false,
+        title: await firstValueFrom(
+          this.translateService.get('customer.tabs.metrics.title')
+        ),
+      },
+    ]
+  }
+
 }
