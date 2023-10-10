c441bd159bc1c652b35e7f5b3a117fe90fbd385e
JoseDias <jose.dias@inovretail.com>
2023-10-10 18:18:18 +0100

    fix: assets not being presented

--- client/web/src/environments/environment.prod.ts
+++ client/web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  i18nURL: `../assets/i18n`, // TODO: replace it with the url to translations
+  i18nURL: `assets/i18n`, // TODO: replace it with the url to translations
   // baseUrl: '/middleware',
   baseUrl: 'https://api.seeplus.inovretail.com', // FIXME: replace with service url
 };
--- client/web/src/environments/environment.ts
+++ client/web/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   defaultLang: `pt-PT`,
-  i18nURL: `../assets/i18n`, // TODO: replace it with the url to translations
+  i18nURL: `/assets/i18n`, // TODO: replace it with the url to translations
   baseUrl: 'https://api.seeplus.inovretail.com',
   // baseUrl: '/middleware',
 };
