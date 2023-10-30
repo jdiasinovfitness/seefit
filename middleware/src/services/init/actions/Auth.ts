ce4147946f3ff37961b98d433fb3679bc29e82d6
flav1o <flavio.costa@inflightit.com>
2023-10-30 16:43:35 +0000

    chore(logs): remove logs

--- middleware/src/services/init/actions/Auth.ts
+++ middleware/src/services/init/actions/Auth.ts
@@ -16,10 +16,9 @@ export default async (
 ): Promise<void> => {
 	try {
 		const authorization = req.headers['authorization'] as string;
-		console.log('Authorization', authorization);
 		const loginResponse = await AuthProvider.login(req, authorization);
-		console.log(loginResponse.accessToken);
 		const authToken = 'Bearer ' + loginResponse.accessToken;
+
 		const userInfo = await UserProvider.userProfile(authToken);
 
 		const user: ILoginResponse = {
