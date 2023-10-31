b127a7f77fb4df88b44197891b1802054ccb40a5
flav1o <flavio.costa@inflightit.com>
2023-10-31 16:42:15 +0000

    add logs

--- middleware/src/services/init/actions/Config.ts
+++ middleware/src/services/init/actions/Config.ts
@@ -6,25 +6,32 @@ export default async (
 	res: Response,
 	next: NextFunction
 ): Promise<void> => {
+	const authToken = req.headers['authorization'] as string;
+	console.log('auth header: ', authToken);
+
 	try {
-		const authToken = req.headers['authorization'] as string;
-		console.log('auth header: ', authToken);
 		const testId = '5c51de7120cc4509e2e941e5';
+		const origin = '5c51c934737aaa0016733c00';
 
 		const userOrigins = await UserProvider.getUserOrigins(testId, authToken);
 		const userPerms = await UserProvider.getUserPermissions(testId, authToken);
-		// const userLocations = await UserProvider.getUserLocations(authToken,);
+		const userLocations = await UserProvider.getUserLocations(
+			authToken,
+			testId,
+			origin
+		);
 
 		const response = {
 			origins: userOrigins,
 			permissions: userPerms,
+			locations: userLocations,
 		};
 
 		console.log(response);
 
 		res.status(200).send(response);
 	} catch (err) {
-		console.log(err);
+		console.log('THIS IS THE ERROR', err);
 		next(err);
 	}
 };
