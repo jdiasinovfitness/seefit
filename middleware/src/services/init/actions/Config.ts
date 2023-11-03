17f33808dcd30910e2f7169b0a99a6e072d9eaec
flav1o <flavio.costa@inflightit.com>
2023-11-03 16:33:49 +0000

    chore(locations): add filtered locations

--- middleware/src/services/init/actions/Config.ts
+++ middleware/src/services/init/actions/Config.ts
@@ -1,6 +1,6 @@
 import UserProvider from '../../provider/User';
 import { NextFunction, Request, Response } from 'express';
-
+import LocationProvider from '../../provider/Location';
 import NormalizeHelper from '../../../helpers/Normalize';
 
 export interface DecodedToken {
@@ -19,7 +19,7 @@ export default async (
 	next: NextFunction
 ): Promise<void> => {
 	const authToken = req.headers['authorization'] as string;
-	// const origin = req.headers['origin'] as string;
+	const origin = req.headers['origin'] as string;
 
 	try {
 		// const decodedToken = await Authentication.decodeToken(authToken);
@@ -28,27 +28,25 @@ export default async (
 		const userId = '5c51de7120cc4509e2e941e5';
 
 		const userOrigins = await UserProvider.getUserOrigins(userId, authToken);
-		// for each origin get the perms and locations
-
-		let locationPromises = [];
-		for (const origin of userOrigins) {
-			const userLocations = UserProvider.getUserLocations(
-				authToken,
-				userId,
-				origin.id
-			);
-
-			locationPromises.push(userLocations);
-		}
-
-		const userLocations = await Promise.all(locationPromises);
-		console.log('USER LOCATIONS ->', userLocations);
-
 		const userPerms = await UserProvider.getUserPermissions(userId, authToken);
+
+		const userLocations = await UserProvider.getUserLocations(
+			authToken,
+			userId,
+			origin
+		);
+		const locationIds = userLocations.map(location => location.location);
+		const locations = await LocationProvider.locationsSummary(authToken, origin);
+		const filteredLocations = locations.filter(location =>
+			locationIds.includes(location.id)
+		);
+
+		console.log(filteredLocations);
+
 		const response = {
 			origins: NormalizeHelper.normalizeOrigins(userOrigins),
 			permissions: NormalizeHelper.normalizePermissions(userPerms),
-			locations: userLocations,
+			locations: filteredLocations,
 		};
 
 		res.status(200).send(response);
--- middleware/src/services/provider/Location.ts
+++ middleware/src/services/provider/Location.ts
@@ -1,9 +1,30 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { processAPIError } from 'utils/httpErrors';
 
-const locationsSummary = async (token: string, origin: string) => {
+export interface Location {
+	id: string;
+	desc: string;
+	zones: { code: string; desc: string }[];
+	origin: string;
+	code: string;
+	type: string;
+	status: string;
+	address: {
+		country: string;
+		city: string;
+		postal_code: string;
+		address: string;
+	};
+	correlation_id: string;
+	description: string;
+}
+
+const locationsSummary = async (
+	token: string,
+	origin: string
+): Promise<Location[]> => {
 	try {
-		const response = await axios.request({
+		const response: AxiosResponse<Location[]> = await axios.request({
 			method: 'GET',
 			url: `${process.env.API_GATEWAY}/location/summary`,
 			headers: {
