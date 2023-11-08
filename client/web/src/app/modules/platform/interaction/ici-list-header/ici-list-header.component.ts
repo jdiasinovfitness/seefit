c709897a4dce15d7004731134996b1f0e56710f6
AzureAD\MykellenPeixoto <mykellen.peixoto@inovretail.com>
2023-11-08 18:20:04 +0000

    feat: order  icons based on info

--- client/web/src/app/modules/platform/interaction/ici-list-header/ici-list-header.component.ts
+++ client/web/src/app/modules/platform/interaction/ici-list-header/ici-list-header.component.ts
@@ -8,8 +8,48 @@ import { Component, Input, OnInit } from '@angular/core';
 export class IciListHeaderComponent implements OnInit {
   @Input() item!: Customer;
   profilePhoto = 'https://ionicframework.com/docs/img/demos/avatar.svg';
+  orderIcons: Array<string> = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.prioritizeIcons();
+  }
+
+  // Sort icons by info priority
+
+  prioritizeIcons() {
+    if (this.item) {
+      let orderIcons = this.item.additionalInfo?.icons || [];
+
+      if (this.item.healthRisk) {
+        orderIcons = this.moveToTop(orderIcons, 'ic-2_1');
+      }
+      const isBirthdayToday = this.isBdayToday(new Date(this.item.dob));
+      if (isBirthdayToday) {
+        orderIcons = this.moveToTop(orderIcons, 'ic-1_1');
+      }
+      if (this.item.healthRisk && isBirthdayToday) {
+        orderIcons = this.moveToTop(orderIcons, 'ic-2_1');
+      }
+      this.orderIcons = orderIcons;
+    }
+  }
+
+  moveToTop(iconArray: string[], icon: string): string[] {
+    const iconIndex = iconArray.indexOf(icon);
+    if (iconIndex !== -1) {
+      iconArray.splice(iconIndex, 1);
+    }
+    iconArray.unshift(icon);
+    return iconArray;
+  }
+
+  isBdayToday(birthday: Date): boolean {
+    const today = new Date();
+    return (
+      birthday.getDate() === today.getDate() &&
+      birthday.getMonth() === today.getMonth()
+    );
+  }
 }
